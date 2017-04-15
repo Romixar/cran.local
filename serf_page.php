9f660ad680a8e8b80ac02c393414be33c2397d39
romixar <you@example.com>
2017-04-15 07:08:10 +0300

    устанавливаю таймер

---
+++ serf_page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Просмотр страницы</title>
+
+  <link rel="stylesheet" href="/css/style.css">
+ </head>
+ <frameset rows="100,*" frameborder='0'>
+  <frame class="test" src="timer.html" name="TIMER" noresize noborder>
+  <frame src="http://zolushka18.ru" name="CONTENT">
+ </frameset>
+</html>
